87b7a5b2b54ff92059c04dc31f6b471d7f9cc8e7
jaewonjs <jaewonshin22@gmail.com>
2021-03-10 18:21:58 -0500

    webpage link in email

--- email_setup.py
+++ email_setup.py
@@ -17,7 +17,6 @@ def sent_email(data):
     #Adjust html to how you want it to look like
     html = """\
     <html>    
-<<<<<<< HEAD
        <h1 style="text-align: center">Today's Meeting</h1>
        <hr>
        <h2 style ="text-align: left;">For meeting <span style="color:#A53FD2; font-weight: normal">{0}</span> on
@@ -33,21 +32,6 @@ def sent_email(data):
        <h2 style ="color:black; text-align: left"> What to focus on for next time: </h2>
        <h3 style ="color:black; text-align: left; font-weight: normal; padding-left: 40px;"> {4}</h3>
        <a href="https://google.com" class="button">Go to Webpage</a>
-=======
-    <h1 style="text-align: center">Today's Meeting</h1>
-    <hr>
-    <h2 style ="text-align: left;">For meeting <span style="color:#A53FD2; font-weight: normal">{0}</span> on
-    <span style="color:#A53FD2; font-weight: normal">{1}</span></h2>
-    <hr>
-    <h2 style ="color:black; text-align: left">Number of Offenses: {2}  </h2>
-    <h2 style ="color:black; text-align: left"> Top Offenses: </h2>
-    <h3 style ="color:#E37100; text-align: left; padding-left: 40px; font-weight: normal"> {3} </h3>
-    <h3 style ="color:#E37100; text-align: left; padding-left: 40px; font-weight: normal"> {4} </h3>
-    <h3 style ="color:#E37100; text-align: left; padding-left: 40px; font-weight: normal"> {5} </h3>
-    <hr>
-    <h2 style ="color:black; text-align: left"> What to focus on for next time: </h2>
-    <h3 style ="color:#E37100; text-align: left; padding-left: 40px; font-weight: normal"> Create a feedback dependent on the data gathered from meeting</h3>
->>>>>>> e6200248f8fcb3641f4ab3fbf472a449a4944df4
     </html>
     """.format('Jun', date, data['count'], data['offenses'][0][0], data['offenses'][1][0],data['offenses'][2][0])
 
